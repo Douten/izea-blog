e1486eccaf696eb8c6019987a885e806734e4d42
Minh Nguyen <minh.n@me.com>
2019-07-14 17:51:44 -0400

    lorem-ipsum generate

--- app/components/post.js
+++ app/components/post.js
@@ -1,10 +1,12 @@
 import Component from '@ember/component';
 import { inject as service } from '@ember/service';
+import { LoremIpsum } from 'lorem-ipsum';
 
 export default Component.extend({
   showBody: false,
   ajax: service(),
   user: null,
+  fullText: null,
   actions: {
     show() {
       this.toggleProperty('showBody');
@@ -17,5 +19,24 @@ export default Component.extend({
       .then(response =>
         this.set('user', response.find(user => this.post.userId === user.id))
       );
+
+    const lorem = new LoremIpsum({
+      sentencesPerParagraph: {
+        max: 8,
+        min: 2
+      },
+      wordsPerSentence: {
+        max: 16,
+        min: 5
+      }
+    });
+
+    const paragaphs = Math.floor(Math.random() * (6 - 2) + 2);
+    const text = [];
+    for (let i = 0; i < paragaphs; i++) {
+      text.push(lorem.generateParagraphs(1));
+    }
+
+    this.fullText = text;
   }
 });
