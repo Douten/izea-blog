575525128223e07349f2a9796f7600cb4e46b0d1
Minh Nguyen <minh.n@me.com>
2019-07-16 13:35:22 -0400

    notes

--- app/routes/index.js
+++ app/routes/index.js
@@ -9,7 +9,20 @@ export default Route.extend({
       this.ajax
         .request('https://jsonplaceholder.typicode.com/posts')
         //randomize
-        .then(response => response.sort(() => Math.random() - 0.5))
+        .then(response => {
+          const style = `background: #25262b; color: #f2ede3; font: 20px/40px 'Literata', serif; padding: 15px; margin: 0; border 0;`;
+
+          console.log(
+            `%cThank you for the opportunity to let me show what I know.`,
+            style
+          );
+          console.log(
+            `%cThere is still a lot for me to learn, but I hope what is there is good.`,
+            style
+          );
+          console.log(`%cIf you have any critiques please let me know!`, style);
+          return response.sort(() => Math.random() - 0.5);
+        })
     );
   }
 });
