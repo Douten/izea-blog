695f3c38d3ae60600077468b97d041a27e74f0ea
Minh Nguyen <minh.n@me.com>
2019-07-16 01:47:27 -0400

    clean up

--- config/environment.js
+++ config/environment.js
@@ -4,7 +4,7 @@ module.exports = function(environment) {
   let ENV = {
     modulePrefix: 'izea-blog',
     environment,
-    rootURL: '/',
+    rootURL: 'https://douten.github.io/izea-blog/',
     locationType: 'auto',
     EmberENV: {
       FEATURES: {
