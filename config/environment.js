28bf8646ea5218f5ab06da2090a01cb8e32805df
Minh Nguyen <minh.n@me.com>
2019-07-16 01:50:25 -0400

    fixed rootURL

--- config/environment.js
+++ config/environment.js
@@ -4,7 +4,7 @@ module.exports = function(environment) {
   let ENV = {
     modulePrefix: 'izea-blog',
     environment,
-    rootURL: 'https://douten.github.io/izea-blog/',
+    rootURL: '/izea-blog',
     locationType: 'auto',
     EmberENV: {
       FEATURES: {
