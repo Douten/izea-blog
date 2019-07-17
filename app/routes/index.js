c96aabab97ad9122636482a6724f90c142bff9e4
Minh Nguyen <minh.n@me.com>
2019-07-16 21:09:02 -0400

    wording

--- app/routes/index.js
+++ app/routes/index.js
@@ -13,14 +13,15 @@ export default Route.extend({
           const style = `background: #25262b; color: #f2ede3; font: 20px/40px 'Literata', serif; padding: 15px; margin: 0; border 0;`;
 
           console.log(
-            `%cI appreciate the opportunity for me to demonstrate my abilities, thank you.`,
+            `%cI appreciate the opportunity for me to demonstrate my abilities.`,
             style
           );
           console.log(
-            `%cThere is still a lot for me to learn, but I think you'll like what you see here.`,
+            `%cThere is still a lot for me to learn, but I believe you'll like what you see here.`,
             style
           );
           console.log(`%cIf you have any critiques please let me know!`, style);
+          console.log(`%cThank you for your time — Minh`, style);
           return response.sort(() => Math.random() - 0.5);
         })
     );
